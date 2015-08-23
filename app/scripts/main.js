3efa5868f65c884a2c39bb76d13800ed36699efd
stephjiayi <steph.jiayi@gmail.com>
2015-08-24 00:52:47 +0200

    Update first step

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,7 @@
-(function () {
-  'use strict';
-  
-})();
+'use strict';
+var viewportWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+if (viewportWidth > 768) {
+  var hwpHeight = document.getElementsByClassName('hwp')[0].offsetHeight,
+    headerHeight = document.getElementsByClassName('header')[0].offsetHeight;
+  document.getElementsByClassName('nav')[0].style.height = hwpHeight - headerHeight + 'px';
+}
