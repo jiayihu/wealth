1f38c5b9a5fc7c0f1d5259f7b640aebc5281324d
stephjiayi <steph.jiayi@gmail.com>
2015-08-25 01:52:43 +0200

    Step templates loading

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,3 +5,33 @@ if (viewportWidth > 768) {
     headerHeight = document.getElementsByClassName('header')[0].offsetHeight;
   document.getElementsByClassName('nav')[0].style.height = hwpHeight - headerHeight + 'px';
 }
+
+// Load the HTML Template of the step from the /templates/ folder
+function loadTemplate(templateUrl) {
+  var mainDiv = document.getElementsByClassName('main')[0],
+    xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4) {
+      console.log(xhr);
+      mainDiv.innerHTML = xhr.response;
+    }
+  };
+  xhr.open('GET', templateUrl);
+  xhr.send();
+}
+
+//Event delegation for changing template
+var nav = document.querySelector('.nav');
+nav.addEventListener('click', function(e) {
+  console.log(e.target);
+  if (e.target.nodeName === 'SPAN') {
+    var templateUrl = e.target.dataset.template,
+      activeLink = document.getElementsByClassName('active')[0];
+    loadTemplate(templateUrl);
+    activeLink.classList.remove('active');
+    e.target.parentNode.classList.add('active');
+  }
+});
+
+//Setting the first step template on load
+loadTemplate('templates/first.html');
