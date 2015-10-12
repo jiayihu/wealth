36f809fe92581a80d99328424e668d647762ac18
stephjiayi <steph.jiayi@gmail.com>
2015-10-12 18:54:27 +0200

    Object literal notation & actions model

--- app/scripts/templates/8-retirement.js
+++ app/scripts/templates/8-retirement.js
@@ -1,78 +1,75 @@
 (function() {
-  var actions = [];
+  var retirementModule = {
+    config: {
+      wrapper: 'retirement-wrapper',
+      jsonUrl: 'scripts/model/actions.json'
+    },
 
-  actions = [
-    {
-      todo: 'Open an account with an online discount broker 0',
-      todonot: 'Hire a full service commission broker',
-      why: 'Explanation for this action'
+    init: function() {
+      retirementModule.wrapper = document.getElementsByClassName(retirementModule.config.wrapper)[0];
+      retirementModule.loadJson(retirementModule.config.jsonUrl);
     },
-    {
-      todo: 'Open an account with an online discount broker 1',
-      todonot: 'Hire a full service commission broker',
-      why: 'Explanation for this action'
+
+    loadJson: function(jsonUrl) {
+      var request = new XMLHttpRequest();
+      request.open('GET', jsonUrl, true);
+      request.onload = function() {
+        if(request.status >=200 && request.status < 400) {
+          retirementModule.data = JSON.parse(request.responseText);
+          retirementModule.createActions();
+
+          //Tooltips
+          $('.retirement-wrapper .zmdi-info-outline').tooltip();
+
+          //Update the model when 'Continue' is pressed
+          retirementModule.continueButton = retirementModule.wrapper.getElementsByClassName('continue')[0];
+          retirementModule.continueButton.addEventListener('click', retirementModule.updateModel);
+        } else {
+          console.log('Error with the connection.');
+        }
+      };
+      request.onerror = function() {
+        console.log('Error with the connection.');
+      };
+      request.send();
     },
-    {
-      todo: 'Open an account with an online discount broker 2',
-      todonot: 'Hire a full service commission broker',
-      why: 'Explanation for this action'
+
+    createActions: function() {
+      var tbody = retirementModule.wrapper.getElementsByTagName('tbody')[0],
+        row;
+
+      retirementModule.data.actions.forEach(function(element, index) {
+        row = document.createElement('tr');
+        row.innerHTML = '<td><i class="zmdi zmdi-check-circle" data-action="' + index + '"></i></td>' +
+          '<td>' + element.todo + '</td>' +
+          '<td>' + element.todonot + '</td>' +
+          '<td><i class="zmdi zmdi-info-outline" data-toggle="tooltip" data-placement="left" title="' + element.why + '"></i></td>';
+          tbody.appendChild(row);
+      });
+
+      retirementModule.checkEventListener();
     },
-    {
-      todo: 'Open an account with an online discount broker 3',
-      todonot: 'Hire a full service commission broker',
-      why: 'Explanation for this action'
+
+    checkEventListener: function() {
+      var checks = retirementModule.wrapper.getElementsByClassName('zmdi-check-circle');
+      Array.prototype.forEach.call(checks, function(element) {
+        element.addEventListener('click', function() {
+          this.classList.toggle('saved');
+        });
+      });
     },
-    {
-      todo: 'Open an account with an online discount broker 4',
-      todonot: 'Hire a full service commission broker',
-      why: 'Explanation for this action'
-    },
-    {
-      todo: 'Open an account with an online discount broker',
-      todonot: 'Hire a full service commission broker',
-      why: 'Explanation for this action'
-    },
-    {
-      todo: 'Open an account with an online discount broker',
-      todonot: 'Hire a full service commission broker',
-      why: 'Explanation for this action'
-    },
-    {
-      todo: 'Open an account with an online discount broker',
-      todonot: 'Hire a full service commission broker',
-      why: 'Explanation for this action'
+
+    updateModel: function() {
+      gModel.savedActions = [];
+      var checkedActions = retirementModule.wrapper.getElementsByClassName('saved');
+      Array.prototype.forEach.call(checkedActions, function(element) {
+        gModel.savedActions.push(retirementModule.data.actions[parseInt(element.dataset.action)]);
+      });
+      console.log(gModel.savedActions);
     }
-  ];
 
-  var retirementWrapper = document.getElementsByClassName('retirement-wrapper')[0],
-    tbody = retirementWrapper.getElementsByTagName('tbody')[0],
-    row;
+  };
 
-  for(var i = 0; i < actions.length; i++) {
-    row = document.createElement('tr');
-    row.innerHTML = '<td><i class="zmdi zmdi-check-circle" data-action="' + i + '"></i></td>' +
-      '<td>' + actions[i].todo + '</td>' +
-      '<td>' + actions[i].todonot + '</td>' +
-      '<td><i class="zmdi zmdi-info-outline" data-toggle="tooltip" data-placement="left" title="' + actions[i].why + '"></i></td>';
-      tbody.appendChild(row);
-  }
+  retirementModule.init();
 
-  var checks = retirementWrapper.getElementsByClassName('zmdi-check-circle');
-  for(var j = 0; j < checks.length; j++) {
-    checks[j].addEventListener('click', function() {
-      this.classList.toggle('saved');
-    });
-  }
-
-  var continueButton = retirementWrapper.getElementsByClassName('continue')[0];
-  continueButton.addEventListener('click', function() {
-    gModel.savedActions = [];
-    var checkedActions = retirementWrapper.getElementsByClassName('saved');
-    for(var k = 0; k < checkedActions.length; k++) {
-      gModel.savedActions.push(actions[parseInt(checkedActions[k].dataset.action)]);
-    }
-    console.log(gModel.savedActions);
-  });
-
-  $('.retirement-wrapper .zmdi-info-outline').tooltip();
 })();
